fd10aa887a3030d302c86e8271d345b11edb7c41
George Thomas <tgeorgetom@gmail.com>
2018-10-13 20:45:16 +0530

    Change of progress

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,21 @@ $(document).ready(function(){
     $('.find_out_btn').click(function(){
         $(".question_footer, .step1").hide();
        	$(".success_footer, .step2").show();
+       	$('.question_progress_bar span').css('width','50%')
+	    $('.second').circleProgress({
+			value: val,
+			startAngle: 4.7,
+			fill: {
+			  gradient: ['#f2c15f', '#e5854f'], 
+			  gradientDirection: [0, 0, 120, 30] 
+			}
+		});
+    
     });
 
+
 });
+
+console.log(Math.PI);
+var val = $(".second").attr("val");
 
